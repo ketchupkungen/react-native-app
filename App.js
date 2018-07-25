2965a57995bc0151419be8773a81947342f3d894
Björn Ohlsson <bjorn.ohlsson.93@gmail.com>
2018-07-25 12:22:57 +0200

    First build

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default class App extends Component {
+  state = {
+    placeName: ''
+  }
+
+  placeNameChangedHandler = (e) => {
+    alert(e);
+    this.setState({
+      placeName: val
+    })
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>My guuuuuuuuurd!!!</Text>
+        <View style={styles.inputContainer}>
+          <Text>My guuuuuuuuurd!!!</Text>
+          <TextInput
+            placeholder="An awesome place" 
+            value={this.state.placeName}
+            onChangeText={this.placeNameChangedHandler}
+            style={{styles.placeInput}}
+          />
+          <Button 
+            title="Add"
+            style={{styles.placeButton}}
+          />
+        </View>
       </View>
     );
   }
@@ -14,8 +37,21 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 20,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start'
   },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: 'space-between'
+    alignItems: "center"
+  },
+  placeInput: {
+    width: "70%"
+  },
+  placeButton: {
+    width: "30%"
+  }
 });
